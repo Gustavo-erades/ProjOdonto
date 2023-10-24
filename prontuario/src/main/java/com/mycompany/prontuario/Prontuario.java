e7887f0f2d7288658a717ebdd995fee0e96bc135
Gustavo-erades <eradesvilarinho@gmail.com>
2023-10-24 18:32:09 -0300

    mudanças no prontuario.java

--- prontuario/src/main/java/com/mycompany/prontuario/Prontuario.java
+++ prontuario/src/main/java/com/mycompany/prontuario/Prontuario.java
@@ -7,18 +7,7 @@ import com.mycompany.prontuario.entidades.Paciente;*/
 public class Prontuario {
 
     public static void main(String[] args){
-        /*Paciente paciente= new Paciente();
-        paciente.setNome("Hal Jordan");
-        paciente.setEmail("piorlanterna@verde.com");
-        paciente.setCpf("0123213413");
-        paciente.setEndereco("OA,Planeta");
-        paciente.setIdade(32);
-        paciente.setRg("12323");
-        paciente.setTelefone("(93)77889-7679");
-        new PacienteDao().cadastrarPaciente(paciente);*/
-        
         TelaCadPaciente tela= new TelaCadPaciente();
         tela.setVisible(true);
-        
     }
 }
