b281f229b528b1315341874a20626863543e3b5e
Gustavo-erades <eradesvilarinho@gmail.com>
2023-10-24 22:30:00 -0300

    atualizando banco

--- prontuariobd.sql
+++ prontuariobd.sql
@@ -12,5 +12,16 @@ alter table prontuariobd.paciente add column rg int(7) not null;
 alter table prontuariobd.paciente add column idade int not null;
 alter table prontuariobd.paciente add column telefone varchar(20);
 alter table prontuariobd.paciente add column email varchar(100);
-alter table prontuariobd.paciente add column endereco varchar(100);*/
-
+alter table prontuariobd.paciente add column endereco varchar(100);
+alter table prontuariobd.paciente add column sexo  varchar(20);
+alter table prontuariobd.paciente add column data_nascimento varchar(10);
+alter table prontuariobd.paciente add column estado_civil varchar(20);
+alter table prontuariobd.paciente add column cor varchar(12);
+alter table prontuariobd.paciente add column proficao varchar(30);
+alter table prontuariobd.paciente add column cep varchar(9);
+alter table prontuariobd.paciente add column telefone_casa varchar(20);
+alter table prontuariobd.paciente add column cidade varchar(20);
+alter table prontuariobd.paciente add column telefone_trab varchar(20);
+alter table prontuariobd.paciente add column responsavel varchar (50);
+alter table prontuariobd.paciente add column telefone_responsavel varchar(20);
+alter table prontuariobd.paciente add column email_responsavel varchar(100);*/
