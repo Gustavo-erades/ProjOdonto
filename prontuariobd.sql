e832d7dc327cbd7dd6e1c083af802e509de5941b
Gustavo-erades <eradesvilarinho@gmail.com>
2023-11-05 12:11:36 -0300

    organizando o script sql

--- prontuariobd.sql
+++ prontuariobd.sql
@@ -3,6 +3,24 @@ use prontuariobd;
 create table paciente(
 	nome varchar(150),
     id int not null auto_increment,
+	cpf int(11) not null,
+	rg int(7) not null,
+    idade int not null,
+    telefone varchar(20),
+    email varchar(100),
+    endereco varchar(100),
+    sexo  varchar(20),
+    data_nascimento varchar(10),
+    estado_civil varchar(20),
+    cor varchar(12),
+    profissao varchar(30),
+    cep varchar(9),
+    telefone_casa varchar(20),
+    cidade varchar(20),
+    telefone_trab varchar(20),
+    responsavel varchar (50),
+    telefone_responsavel varchar(20),
+    email_responsavel varchar(100),
     primary key (id)
 );
 create table prontuariobd.paciente_informacoes(
@@ -21,24 +39,4 @@ create table prontuariobd.paciente_informacoes(
 	foreign key (id_paciente) references prontuariobd.paciente(id)
 );
 
-select *from prontuariobd.paciente;/*		
-insert into prontuariobd.paciente(nome) values("gustavo");
-alter  table prontuariobd.paciente add column cpf int(11) not null;
-alter table prontuariobd.paciente add column rg int(7) not null;
-alter table prontuariobd.paciente add column idade int not null;
-alter table prontuariobd.paciente add column telefone varchar(20);
-alter table prontuariobd.paciente add column email varchar(100);
-alter table prontuariobd.paciente add column endereco varchar(100);
-alter table prontuariobd.paciente add column sexo  varchar(20);
-alter table prontuariobd.paciente add column data_nascimento varchar(10);
-alter table prontuariobd.paciente add column estado_civil varchar(20);
-alter table prontuariobd.paciente add column cor varchar(12);
-alter table prontuariobd.paciente add column proficao varchar(30);
-alter table prontuariobd.paciente add column cep varchar(9);
-alter table prontuariobd.paciente add column telefone_casa varchar(20);
-alter table prontuariobd.paciente add column cidade varchar(20);
-alter table prontuariobd.paciente add column telefone_trab varchar(20);
-alter table prontuariobd.paciente add column responsavel varchar (50);
-alter table prontuariobd.paciente add column telefone_responsavel varchar(20);
-alter table prontuariobd.paciente add column email_responsavel varchar(100);
-alter table prontuariobd.paciente change proficao profissao varchar(100);*/
+select *from prontuariobd.paciente;
