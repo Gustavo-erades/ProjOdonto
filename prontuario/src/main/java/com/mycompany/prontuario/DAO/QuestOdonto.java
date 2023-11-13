55d03e05a7a4d2a8e2c4d7e20a5094334d4a0c66
Gustavo-erades <eradesvilarinho@gmail.com>
2023-11-13 15:24:18 -0300

    começando a criar a classe de entidade das questoões odontológicas

---
+++ prontuario/src/main/java/com/mycompany/prontuario/DAO/QuestOdonto.java
@@ -0,0 +1,6 @@
+
+package com.mycompany.prontuario.DAO;
+
+public class QuestOdonto {
+    
+}
---
+++ prontuario/src/main/java/com/mycompany/prontuario/entidades/QuestOdonto.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.prontuario.entidades;
+
+public class QuestOdonto {
+    private String questoes;
+    public String getQuestoes(){
+        return questoes;
+    }
+    private String implante;
+    private String tempo;
+    
+}
